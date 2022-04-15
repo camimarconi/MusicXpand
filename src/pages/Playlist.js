2df9d0deea12a43f0d497d7fee8e418823e3831a
Camila Solér <msoler.camila@gmail.com>
2022-04-14 23:19:08 -0300

    mini alterações playlist

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -42,27 +42,23 @@ function Playlist() {
               aria-current="true"
             >
               <img
-                src={musicXpandListApi.albumCover}
+                src={current.albumCover}
                 alt="twbs"
                 width="50"
                 height="50"
                 className="rounded-circle flex-shrink-0"
               />
               <div className="d-flex gap-2 w-100 justify-content-evenly align-items-center align-self-center">
-                <h5 className="mb-0 fw-bold">{musicXpandListApi.songName}</h5>
-                {/* <h5 className="mb-0 opacity-75">
-                  {musicXpandListApi.artistName.map((current) => {
+                <h5 className="mb-0 fw-bold">{current.songName}</h5>
+                <h5 className="mb-0 opacity-75">
+                  {current.artistName.map((current) => {
                     return current.name;
                   })}
-                </h5> */}
-                <h5 className="mb-0 opacity-50 fw-light">
-                  {musicXpandListApi.albumName}
                 </h5>
-                <audio
-                  className="opacity-50 text-nowrap"
-                  controls
-                  src={musicXpandListApi.preview_url}
-                ></audio>
+                <h5 className="mb-0 opacity-50 fw-light">
+                  {current.albumName}
+                </h5>
+                <audio controls src={current.musicPreview}></audio>
                 <h4>DELETE</h4>
               </div>
             </li>
